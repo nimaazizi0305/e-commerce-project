72cc31a503753ec3b667f82f044f469494e6c684
nimaazizi0305 <80009626+nimaazizi0305@users.noreply.github.com>
2023-04-16 13:20:31 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -3,11 +3,3 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.views import View
 
-# Create your models here.
-
-# class UserView(AbstractUser):
-#     avatar=models.ImageField(upload_to="static/images/profile",null=True,blank=True)
-#     def __str__(self):
-#         return  self.username
-#
-# # Create your models here.
