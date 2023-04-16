9e0956a8768afe3573564e76b4208f2a430307ec
nimaazizi0305 <80009626+nimaazizi0305@users.noreply.github.com>
2023-04-16 13:21:08 -0500

    Update views.py

--- account/views.py
+++ account/views.py
@@ -61,20 +61,6 @@ def login(request):
     return render(request,"account/signin.html",context)
 
 
-# def register(request:HttpRequest):
-#     if request.method == "POST":
-#         form=UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username=form.cleaned_data.get("username")
-#             messages.success(request,f"User {username} Created Now")
-#             return redirect("login")
-#     else:
-#         form=UserCreationForm()
-#     context={
-#         "form":form
-#     }
-#     return render(request,"account/register.html",context)
 
 @login_required(login_url="login")
 def dashbord(request):
